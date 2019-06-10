d386b5a796f930c538028cdd3347462b8c467a22
Luis Hernández <luis_hernandez@epam.com>
2019-06-10 13:05:12 -0500

    Implement logic for reactive add todo

--- src/app/components/rx-add-todo/rx-add-todo.component.ts
+++ src/app/components/rx-add-todo/rx-add-todo.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subject, SubscriptionLike } from 'rxjs';
+import { debounceTime, map, filter } from 'rxjs/operators';
+
+import { TodoItem } from 'src/app/models/todo.model';
 
 @Component({
   selector: 'app-rx-add-todo',
   templateUrl: './rx-add-todo.component.html',
-  styleUrls: ['./rx-add-todo.component.scss']
+  styleUrls: ['./rx-add-todo.component.scss'],
 })
-export class RxAddTodoComponent implements OnInit {
+export class RxAddTodoComponent implements OnInit, OnDestroy {
+  @Output()
+  create = new EventEmitter<TodoItem>();
+  todoText: string;
+  userInput$ = new Subject<string>();
 
-  constructor() { }
+  private userInputSub: SubscriptionLike;
 
   ngOnInit() {
+    this.userInputSub = this.userInput$
+      .pipe(
+        debounceTime(500),
+        filter(value => !!value),
+        map(value => value)
+      )
+      .subscribe(val => {
+        this.create.emit({ text: val, done: false });
+      });
   }
 
+  ngOnDestroy() {
+    this.userInputSub.unsubscribe();
+  }
 }
